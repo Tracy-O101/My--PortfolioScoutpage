b506e67f90c7ea1b75996303d97ee74299e3fc81
Tracy-O101 <tracydubaemi@gmail.com>
2026-05-10 08:51:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Tracy Oshonwoh
+// script.js
+// March 3, 2026
+
+// jQuery for hero image to consume the header window space
+$(document).ready(function () {
+    $('.hero').height($(window).height());
+});
